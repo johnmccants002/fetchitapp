4fce796a6ac460de354adfc3ed6903347aab88a2
John McCants <47401369+johnmccants002@users.noreply.github.com>
2023-10-16 20:00:28 -0700

    bug fix

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -20,7 +20,6 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerStyle: { backgroundColor: "#57aaf7" },
       }}
     >
